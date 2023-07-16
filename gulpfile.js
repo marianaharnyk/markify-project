f48dcd18b8ccaf2d6cf357238dbaf25346fc6b3e
marianaharnyk <mariana.harnyk@gmail.com>
2023-07-16 13:40:41 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict'
+const { src, dest, watch, parallel, series } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+function styles () {
+    return src('app/scss/style.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version']
+        }))
+        .pipe(concat('style.min.scss'))
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
+}
+
+function watching(){
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+    watch(['app/scss/*.scss'], styles);
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+function building (){
+    return src([
+        'app/css/style.min.css',
+        'app/**/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+function cleanDist (){
+    return src('dist')
+        .pipe(clean())
+}
+
+exports.styles = styles;
+exports.watching = watching;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, watching);
